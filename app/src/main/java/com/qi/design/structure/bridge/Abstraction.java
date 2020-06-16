ae3948e50ce7307946df12a6484e092a09f2bc9d
qixu <qixu@lenovo-ar.com>
2020-06-16 15:19:40 +0800

    1

--- app/src/main/java/com/qi/design/structure/bridge/Abstraction.java
+++ app/src/main/java/com/qi/design/structure/bridge/Abstraction.java
@@ -7,6 +7,10 @@ package com.qi.design.structure.bridge;
  *
  * 这个抽象类假设是手机ide意思
  *
+ * 当一个类有多个维度的时候 那么就需要用桥接模式
+ * 减少继承 多用关联
+ * 比如  咖啡 有大杯 小杯  有糖无糖
+ *
  */
 public  abstract class Abstraction {
     Implementor implementor;
